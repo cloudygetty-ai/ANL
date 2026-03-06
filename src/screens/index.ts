948a3e4b59cdcf4ebed8321701917775bd41ee82
Cloudygetty <cloudygetty@gmail.com>
2026-03-06 07:11:18 -0500

    feat: screens barrel — all 4 screens

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,2 +1,4 @@
-export { default as HomeScreen } from './HomeScreen';
-export { default as MapScreen }  from './MapScreen';
+export { default as HomeScreen  } from './HomeScreen';
+export { default as MapScreen   } from './MapScreen';
+export { default as ChatScreen  } from './ChatScreen';
+export { default as VideoScreen } from './VideoScreen';

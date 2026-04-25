8ec8a46c1e302fb99f02cac197d8ca950bbcf835
Cloudygetty <cloudygetty@gmail.com>
2026-04-25 16:50:35 -0400

    feat: web – /tmp/anl-fix/vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    target: 'esnext',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
+  server: {
+    port: 5173,
+  },
+});
